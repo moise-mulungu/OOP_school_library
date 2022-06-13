8b9dd5bd79728780e20476bfea499ff71fa95b48
moise <moisemlg90@gmail.com>
2022-06-13 21:48:53 +0200

    Create class Teacher with its data

---
+++ teacher.rb
@@ -0,0 +1,14 @@
+require "./person.rb"
+
+class Teacher < Person
+    def initialize(name = "Unknown", age, parent_permission = true, specialization)
+        super(name, age, parent_permission)
+        @specialization = specialization
+    end
+
+    def can_use_services?
+        true
+    end
+end
+
+teacher = Teacher.new
