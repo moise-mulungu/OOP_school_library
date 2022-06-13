11113b76f5255c0fbf13a76cec276aba6b2892e3
moise <moisemlg90@gmail.com>
2022-06-13 21:37:09 +0200

    Create class Person with its data

---
+++ person.rb
@@ -0,0 +1,33 @@
+class Person
+    def initialize(name = "Unknown", age, parent_permission = true)
+        @id = Random.new.rand(1..100)
+        @name = name
+        @age = age
+    end
+
+    def parent_permission
+        @parent_permission
+    end
+
+    attr_reader :id, :name, :age
+    attr_writer :name, :age
+
+    private
+    def is_of_age?
+        if @age >= 18
+            true
+        else
+            false
+        end
+    end
+
+    def can_use_services?
+        if @age >= 18 && @parent_permission
+            true
+        else
+            false
+        end
+    end
+end
+
+person = Person.new
