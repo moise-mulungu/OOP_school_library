3e221e4d872dc8554601df66d3dcd70affdbbaa0
moise <moisemlg90@gmail.com>
2022-06-15 09:59:32 +0200

    Fix required changes from the reviewer

--- decorators.rb
+++ decorators.rb
@@ -1,7 +1,7 @@
 require './nameable'
 
 # Prepare a base decorator class
-class BaseDecorator
+class BaseDecorator < Nameable
   attr_accessor :nameable
 
   def initialize(nameable)
@@ -24,6 +24,18 @@ end
 # Prepare a trimmer decorator class
 class TrimmerDecorator < BaseDecorator
   def correct_name
-    @nameable.correct_name[0..10]
+    @word_length = @nameable.correct_name.length
+    @word_trim = @nameable.correct_name
+    if @word_length <= 10
+      puts @word_trim
+    else
+      (0..@word_length).each do |_i|
+        next unless @word_length > 10
+
+        @word_trim = @word_trim.chop
+        @word_length = @word_trim.length
+        puts @word_trim
+      end
+    end
   end
 end
--- person.rb
+++ person.rb
@@ -1,4 +1,5 @@
 require './nameable'
+require './decorators'
 
 class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -13,7 +14,12 @@ class Person < Nameable
   attr_reader :id
 
   def can_use_services?
-    is_of_age? || @parent_permission
+    of_age? || @parent_permission
+  end
+
+  # Adding a correct_name class
+  def correct_name
+    @name
   end
 
   private
@@ -21,9 +27,11 @@ class Person < Nameable
   def of_age?
     @age >= 18
   end
-
-  # Adding a correct_name class
-  def correct_name
-    @name = name
-  end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
