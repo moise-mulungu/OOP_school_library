382618bf363027844f6cb263c810cadd107f1275
moise <moisemlg90@gmail.com>
2022-06-16 09:49:41 +0200

    Update change required by the reviewer

--- rental.rb
+++ rental.rb
@@ -1,5 +1,5 @@
 class Rental
-  attr_reader :book, :person
+  attr_accessor :book, :person
   attr_accessor :date
 
   def initialize(date, person, book)
--- student.rb
+++ student.rb
@@ -3,12 +3,17 @@ require './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true, classroom = nil)
     super(age, name, parent_permission)
-    @classroom = nil
+    @classroom = classroom
   end
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def classroom (classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
