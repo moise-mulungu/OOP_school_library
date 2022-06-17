e04c47b5815b33cb2227b0ed9a5064111c3933fe
moise <moisemlg90@gmail.com>
2022-06-17 04:33:02 +0200

    Create all related methods in the app file

--- app.rb
+++ app.rb
@@ -6,8 +6,8 @@ require './rental'
 
 class App
   def initialize
-    @my_books = []
-    @my_rentals = []
+    @books = []
+    @rentals = []
     @people = []
   end
 
@@ -46,3 +46,143 @@ class App
     end
     
   end
+
+#   CREATE METHODS FOR EACH ACTION
+# list all books
+  def list_books
+    @books.each_with_index do |a, index|
+      puts "#{index}) Title: \"#{a.title}\", Author: #{a.author} "
+    end
+  end
+
+# list all people
+  def list_people
+    @people.each_with_index do |a, index|
+      puts "#{index}) [#{a.class.name}] Name: #{a.name}, ID: #{a.id}, Age: #{a.age}"
+    end
+  end
+
+#   All listed books
+  def action_list_books
+    list_books
+    puts 'Press enter to continue ...'
+    gets.chomp
+    run
+  end
+
+#   All listed_people
+  def action_list_people
+    list_people
+    puts "\n\nPress any key to continue"
+    gets
+    run
+  end
+
+#   Asking for permission
+  def my_permission(my_char)
+    if my_char == 'Y'
+        true
+    elsif my_char == 'N'
+        false
+    else
+        puts 'Invalid choice. Please try again.'
+        my_permission(gets.chomp)
+    end
+    # case my_char
+    # when 'N'
+    #   false
+    # when 'Y'
+    #   true
+    # end
+  end
+
+#   create a teacher
+  def create_teacher
+    print 'Age: '
+    age = gets.chomp
+    print 'Name: '
+    name = gets.chomp
+    print 'Specialization: '
+    specialization = gets.chomp
+    teacher = Teacher.new(age, name, nil, specialization)
+    @people.push(teacher)
+  end
+
+#   create a student
+  def create_student
+    print 'Age: '
+    age = gets.chomp
+    print 'Name: '
+    name = gets.chomp
+    print 'Has parent permission? [Y/N]: '
+    permission = gets.chomp
+    student = Student.new(age, name, my_permission(permission), nil)
+    @people.push(student)
+  end
+
+#   create a person
+  def create_person
+    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+    person_type = gets.chomp
+    if person_type == '1'
+        create_student
+    elsif person_type == '2'
+        create_teacher
+    else
+        puts 'Invalid choice. Please try again.'
+        create_person
+    end
+
+    # case person_type
+    # when '1'
+    #   create_student
+    # when '2'
+    #   create_teacher
+    # end
+    # puts "Person created successfully\n"
+    # gets
+    # run
+  end
+
+#   create a book
+  def create_book
+    print 'Title: '
+    title = gets.chomp
+    print 'Author: '
+    author = gets.chomp
+    new_book = Book.new(title, author)
+    @books.push(new_book)
+    run
+  end
+
+#   create a rental
+  def create_rental
+    puts "\nSelect a book from the following list by number"
+    list_books
+    book_index = gets.chomp
+    puts "\nSelect a person from the following list by number"
+    list_people
+    person_index = gets.chomp
+    print "\n Date(yyyy/mm/dd): "
+    rental_date = gets.chomp
+    new_rental = Rental.new(rental_date, @my_books[book_index.to_i], @people[person_index.to_i])
+    @rentals.push(new_rental)
+    puts 'Rental added successfully'
+    run
+  end
+
+#   list all rentals for a given person id
+  def list_rental
+    me = nil
+    print "\nID of person: "
+    person_id = gets.chomp
+    @people.each do |x|
+      peoples = x if x.id == person_id.to_i
+    end
+    peoples.rentals.each do |x|
+      puts x.date
+    end
+    puts
+    run
+end
+end
